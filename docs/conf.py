8e3ca9cbb51833b8bd8b23e5611c35606b210958
Fergus Cooper <fcooper8472@gmail.com>
2023-09-15 15:29:08 +0100

    Try building the project

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,8 @@
 
 import os
 import sys
-# sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
-sys.path.insert(0, os.path.abspath(os.path.join('..', 'arg_needle_lib')))
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
+# sys.path.insert(0, os.path.abspath(os.path.join('..')))
 
 
 # -- Project information -----------------------------------------------------

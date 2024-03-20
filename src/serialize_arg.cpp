4437272bfaa4ce28c23c28a789943fb71e49936a
Fergus Cooper <3770306+fcooper8472@users.noreply.github.com>
2024-03-20 10:40:23 +0000

    Implement remaining file validation

--- src/serialize_arg.cpp
+++ src/serialize_arg.cpp
@@ -43,40 +43,83 @@ bool validate_serialized_arg_v1(const std::string& file_name)
     return false;
   }
 
-  bool isValid = true;
+  bool is_valid = true;
 
   // Check for the presence of expected attributes
   for (const auto& attr : expected_attrs) {
     if (H5Aexists(file_id, attr.c_str()) <= 0) {
       std::cerr << "Expected file " << file_name << " to include attribute `" << attr << "`" << std::endl;
-      isValid = false;
-      break; // Exit the loop early as the file is already invalid
+      is_valid = false;
     }
   }
 
-  if (isValid) {
-    // Check for the presence of expected datasets only if attributes are valid
-    for (const auto& dset : expected_dsets) {
-      const hid_t dset_id = H5Dopen2(file_id, dset.c_str(), H5P_DEFAULT);
-      if (dset_id < 0) {
-        std::cerr << "Expected file " << file_name << " to include dataset `" << dset << "`" << std::endl;
-        isValid = false;
-        break; // Exit the loop early as the file is already invalid
-      }
-      H5Dclose(dset_id); // Close the dataset to clean up resources
+  // Check for the presence of expected datasets only if attributes are valid
+  for (const auto& dset : expected_dsets) {
+    if (const hid_t dset_id = H5Dopen2(file_id, dset.c_str(), H5P_DEFAULT); dset_id < 0) {
+      std::cerr << "Expected file " << file_name << " to include dataset `" << dset << "`" << std::endl;
+      is_valid = false;
+    } else {
+      H5Dclose(dset_id);
     }
   }
 
-  // Close the HDF5 file
   H5Fclose(file_id);
-
-  return isValid;
+  return is_valid;
 }
-
 
 bool validate_serialized_arg_v2(const std::string& file_name)
 {
-  return true;
+  // Expected attributes and datasets
+  std::vector<std::string> expected_attrs = {"num_nodes", "num_edges", "node_bounds", "num_mutations", "mutations",
+      "offset", "chromosome", "start", "end", "threaded_samples", "datetime_created", "arg_file_version"};
+  std::vector<std::string> expected_dsets = {"flags", "times", "edge_ranges", "edge_ids"};
+  std::vector<std::string> optional_dsets = {"mutations", "node_bounds"};
+
+  // Open the HDF5 file
+  const hid_t file_id = H5Fopen(file_name.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
+  if (file_id < 0) {
+    std::cerr << "Could not open file: " << file_name << std::endl;
+    return false;
+  }
+
+  bool is_valid = true;
+
+  // Check for the presence of expected attributes
+  for (const auto& attr : expected_attrs) {
+    if (H5Aexists(file_id, attr.c_str()) <= 0) {
+      std::cerr << "Expected file " << file_name << " to include attribute `" << attr << "`" << std::endl;
+      is_valid = false;
+    }
+  }
+
+  // Check for the presence of optional datasets, appending them to the list of expected datasets if appropriate
+  for (const auto& dset_name : optional_dsets) {
+    if (const hid_t attr_id = H5Aopen(file_id, dset_name.c_str(), H5P_DEFAULT); attr_id < 0) {
+      std::cerr << "Unable to open `" << dset_name << "` attribute in file: " << file_name << std::endl;
+      is_valid = false;
+    } else {
+      int dset_flag = 0;
+      H5Aread(attr_id, H5T_NATIVE_INT, &dset_flag);
+      H5Aclose(attr_id);
+      if (dset_flag) {
+        expected_dsets.emplace_back(dset_name);
+      }
+    }
+  }
+
+  // Check that all expected datasets exist
+  for (const auto& dset : expected_dsets) {
+    const hid_t dset_id = H5Dopen2(file_id, dset.c_str(), H5P_DEFAULT);
+    if (dset_id < 0) {
+      std::cerr << "Expected file " << file_name << " to include dataset `" << dset << "`" << std::endl;
+      is_valid = false;
+    } else {
+      H5Dclose(dset_id);
+    }
+  }
+
+  H5Fclose(file_id);
+  return is_valid;
 }
 } // namespace
 

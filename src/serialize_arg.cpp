162de9991ffb76f78bab219d2646431cc583f9fc
Fergus Cooper <3770306+fcooper8472@users.noreply.github.com>
2024-04-05 13:56:57 +0100

    Add mutations to deserialization

--- src/serialize_arg.cpp
+++ src/serialize_arg.cpp
@@ -320,12 +320,11 @@ ARG deserialize_arg_v2(const H5::H5File& h5_file, const int chunk_size, const in
       const hssize_t range_lo = num_nodes_written;
       const hssize_t range_hi = std::min(num_nodes_written + chunk_size, num_nodes);
 
-      const std::vector<double> node_heights = read_dataset_to_vector_1d<double>(h5_file, "times", range_lo, range_hi);
-      const std::vector<uint8_t> is_sample = read_dataset_to_vector_1d<uint8_t>(h5_file, "flags", range_lo, range_hi);
+      const auto node_heights = read_dataset_to_vector_1d<double>(h5_file, "times", range_lo, range_hi);
+      const auto is_sample = read_dataset_to_vector_1d<uint8_t>(h5_file, "flags", range_lo, range_hi);
 
       if (read_bool_attribute(h5_file, "node_bounds")) {
-        const std::vector<std::array<double, 2>> node_bounds_data =
-            read_dataset_to_vector_2d<double>(h5_file, "node_bounds", range_lo, range_hi);
+        const auto node_bounds_data = read_dataset_to_vector_2d<double>(h5_file, "node_bounds", range_lo, range_hi);
         arg.deserialize_add_nodes(node_heights, is_sample, node_bounds_data);
       } else {
         arg.deserialize_add_nodes(node_heights, is_sample);
@@ -345,15 +344,34 @@ ARG deserialize_arg_v2(const H5::H5File& h5_file, const int chunk_size, const in
       const hssize_t range_lo = num_edges_written;
       const hssize_t range_hi = std::min(num_edges_written + chunk_size, num_edges);
 
-      const std::vector<std::array<int, 2>> edge_id_data =
-          read_dataset_to_vector_2d<int>(h5_file, "edge_ids", range_lo, range_hi);
-
-      const std::vector<std::array<double, 2>> edge_range_data =
-          read_dataset_to_vector_2d<double>(h5_file, "edge_ranges", range_lo, range_hi);
+      const auto edge_id_data = read_dataset_to_vector_2d<int>(h5_file, "edge_ids", range_lo, range_hi);
+      const auto edge_range_data = read_dataset_to_vector_2d<double>(h5_file, "edge_ranges", range_lo, range_hi);
 
       arg.deserialize_add_edges(edge_id_data, edge_range_data);
 
       num_edges_written += static_cast<hssize_t>(edge_id_data.size());
+    }
+  }
+
+  // Process {chunk_size} mutations at a time, adding each chunk to the ARG as we go
+  if (read_bool_attribute(h5_file, "mutations")) {
+
+    const auto num_mutations = static_cast<hssize_t>(read_int_attribute(h5_file, "num_mutations"));
+    hssize_t num_mutations_written = 0;
+
+    while (num_mutations_written < num_mutations) {
+
+      const hssize_t range_lo = num_mutations_written;
+      const hssize_t range_hi = std::min(num_mutations_written + chunk_size, num_mutations);
+
+      const auto mut_pos = read_dataset_to_vector_1d<double>(h5_file, "mutations/positions", range_lo, range_hi);
+      const auto mut_hts = read_dataset_to_vector_1d<double>(h5_file, "mutations/heights", range_lo, range_hi);
+      const auto mut_sid = read_dataset_to_vector_1d<int>(h5_file, "mutations/site_ids", range_lo, range_hi);
+      const auto mut_eid = read_dataset_to_vector_2d<int>(h5_file, "edge_ids", range_lo, range_hi);
+
+      arg.deserialize_add_mutations(mut_pos, mut_hts, mut_sid, mut_eid);
+
+      num_mutations_written += static_cast<hssize_t>(mut_pos.size());
     }
   }
 
